ad7d348f159ce9542ad231a5366c172dae4d02ea
Mattias Fält <mattias.falt@umu.se>
2019-10-28 09:59:43 +0100

    Web.Api: Lade till några interface för tjänster som kommer att behövas

---
+++ LotachampCore/Lotachamp.Application/Interfaces/ILoggerService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lotachamp.Application.Interfaces
+{
+    public interface ILoggerService
+    {
+
+        void LogInfo(Type caller, string message);
+
+        void LogInfo(Type caller, string message, params object[] args);
+
+        void LogError(Type caller, string message, params object[] args);
+
+        void LogError(Type caller, Exception ex, string message, params object[] args);
+
+        void LogError(Type caller, Exception ex, string message = null);
+
+        void LogWarn(Type caller, Exception ex, string message = null);
+
+        void LogWarn(Type caller, string message);
+
+        void LogWarn(Type caller, string message, params object[] args);
+
+        void LogDebug(Type caller, string message, params object[] args);
+
+        void LogDebug(Type caller, string message);
+    }
+}
